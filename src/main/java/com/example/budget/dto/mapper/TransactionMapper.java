d1246cf8f465012513137cf24c54b500bc60aa27
w65458 <w65458@student.wsiz.edu.pl>
2024-06-09 22:32:38 +0200

    Implement TransactionService

--- src/main/java/com/example/budget/dto/mapper/TransactionMapper.java
+++ src/main/java/com/example/budget/dto/mapper/TransactionMapper.java
@@ -3,6 +3,7 @@ package com.example.budget.dto.mapper;
 import com.example.budget.dto.TransactionDto;
 import com.example.budget.entity.Transaction;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -10,6 +11,8 @@ public interface TransactionMapper {
 
     TransactionMapper INSTANCE = Mappers.getMapper(TransactionMapper.class);
 
+    @Mapping(source = "category.id", target = "categoryId")
+    @Mapping(source = "user.id", target = "userId")
     TransactionDto transactionToTransactionDto(Transaction transaction);
     Transaction transactionDtoToTransaction(TransactionDto transactionDto);
 }
--- src/main/java/com/example/budget/service/TransactionServiceImpl.java
+++ src/main/java/com/example/budget/service/TransactionServiceImpl.java
@@ -1,21 +1,57 @@
 package com.example.budget.service;
 
 import com.example.budget.dto.TransactionDto;
+import com.example.budget.dto.mapper.TransactionMapper;
+import com.example.budget.entity.PaymentCategory;
+import com.example.budget.entity.Transaction;
+import com.example.budget.entity.User;
+import com.example.budget.repository.PaymentCategoryRepository;
+import com.example.budget.repository.TransactionRepository;
+import com.example.budget.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class TransactionServiceImpl implements TransactionService {
+
+    private final TransactionRepository transactionRepository;
+
+    private final UserRepository userRepository;
+
+    private final PaymentCategoryRepository paymentCategoryRepository;
 
     @Override
     public TransactionDto createTransaction(TransactionDto transactionDto) {
-        return null;
+        Optional<User> user = userRepository.findById(transactionDto.getUserId());
+        Optional<PaymentCategory> category = paymentCategoryRepository.findById(transactionDto.getCategoryId());
+
+        if (user.isPresent() && category.isPresent()) {
+            Transaction transaction = Transaction.builder()
+                    .user(user.get())
+                    .category(category.get())
+                    .amount(transactionDto.getAmount())
+                    .type(transactionDto.getType())
+                    .description(transactionDto.getDescription())
+                    .transactionDate(transactionDto.getTransactionDate())
+                    .build();
+            Transaction savedTransaction = transactionRepository.save(transaction);
+            return TransactionMapper.INSTANCE.transactionToTransactionDto(savedTransaction);
+        } else {
+            throw new RuntimeException("User or Category not found");
+        }
     }
 
     @Override
     public List<TransactionDto> getAllTransactionsByUserId(Long userId) {
-        return List.of();
-    }
+        List<Transaction> transactions = transactionRepository.findByUserId(userId);
 
+        return transactions.stream()
+                .map(TransactionMapper.INSTANCE::transactionToTransactionDto)
+                .collect(Collectors.toList());
+    }
 }
