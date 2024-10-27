1c03761eeac40100d28df90d5f5582ea56e6489f
Michal Borla <mborla@atlassian.com>
2024-10-27 11:56:12 +0100

    w65458: Replace Header with Sidebar

--- src/components/PageTemplate.jsx
+++ src/components/PageTemplate.jsx
@@ -1,16 +1,27 @@
-import {Header} from "./Header.jsx";
-import {Footer} from "./Footer.jsx";
+import {Sidebar} from './Sidebar.jsx';
+import {Footer} from './Footer.jsx';
+import {useState} from "react";
+import {Bars3BottomRightIcon} from "@heroicons/react/24/outline";
 
-export const PageTemplate = ({ children }) => {
+export const PageTemplate = ({children}) => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <>
-        <div className="flex flex-col h-[calc(100vh-5rem)] items-center mt-20 justify-center place-items-center w-full scrollbar-none">
-         <Header />
-            <div className="flex flex-col h-full bg-gray-100 items-center justify-center place-items-center w-full overflow-hidden scrollbar-none">
-                {children}
+        <div className="flex h-screen">
+            <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+            <div className="flex-grow flex flex-col">
+                {!isOpen && (
+                    <div className="md:hidden absolute top-4 left-4">
+                        <button onClick={() => setIsOpen(true)} className="p-2 bg-blue-600 text-white rounded">
+                            <Bars3BottomRightIcon className="w-6 h-6" />
+                        </button>
+                    </div>
+                )}
+                <div className="flex-grow h-full bg-gray-100 items-center justify-center place-items-center overflow-hidden scrollbar-none">
+                    {children}
+                </div>
+                <Footer />
             </div>
-         <Footer />
         </div>
-        </>
-    )
-}
+    );
+};
